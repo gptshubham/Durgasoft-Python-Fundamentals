fd9e7dda06265f7a28a13793c12923453fae5db5
ShubhamKumarGupta <shubhamca76@gmail.com>
2023-09-29 18:13:24 +0530

    initial commit

---
+++ DataTypes.py
@@ -0,0 +1,30 @@
+'''
+# int
+# Decimal
+a = 1111
+print(a)
+# Binary
+x = 0b1111
+print(x)
+y = 0B1111
+print(y)
+# Octal
+print(0o123)
+# print(0o786)
+print(0x10)
+# Hexadecimal
+print(0xFACE)
+
+
+# Bace Conversion
+print(bin(15))
+print(0o123)
+print(bin(0xface))
+
+print(oct(100))
+print(oct(0b111101))
+print(0XFace)
+'''
+print(hex(1000))
+print(hex(0b10111111))
+print(hex(0o123456))
