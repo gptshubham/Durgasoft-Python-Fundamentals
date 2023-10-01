7d198992581b09ba7fe8a1d3a6cc1d6368e186aa
ShubhamKumarGupta <shubhamca76@gmail.com>
2023-10-01 14:04:48 +0530

    data types topic completes today

--- DataTypes1.py
+++ DataTypes1.py
@@ -4,11 +4,11 @@ print(s[0])
 print(s[3])
 # print(s[100])
 print(s[-1])
-#print(s[-10])
+# print(s[-10])
 print(s[-7])
 
 # slice
-s = 'shubhamkumargupta'
+s = "shubhamkumargupta"
 print(s[7:12])
 print(s[:7])
 print(s[12:])
@@ -32,3 +32,19 @@ s = 'gupta'
 print(s)
 s = s[0].upper() + s[1:len(s)-1] + s[-1].upper()
 print(s)
+
+# + operator
+print('shubh' + 'am')
+# print('shubh' + 10)
+print('shubh' + '10')
+
+# * operator
+print('shubh' * 3)
+# print('shubh' * '3')
+# print('shubh' * 3.3)
+print(3 * 'shubh')
+print('#' * 9)
+print(' shubham')
+print('#' * 9)
+print('shubh' * False)
+print('shubh' * True)
