2582eeae41a7056939bc9fa6142521927b01216b
ShubhamKumarGupta <shubhamca76@gmail.com>
2023-10-03 18:08:56 +0530

    includes sequence data types in Python

--- Immutability.py
+++ Immutability.py
@@ -68,3 +68,66 @@ L.remove(30)
 print(L)
 L[0] = 7777
 print(L)
+
+# Tuple Data Type
+t = (10,20,30,10,'shubh')
+print(type(t))
+print(t[0])
+print(t[1:4])
+print(t[-1])
+print(t[1:])
+print(t[:3])
+# t[0] = 7777
+# t.append(50)
+# t.remove(10)
+t = ()
+print(t,type(t))
+t = (10)
+print(t,type(t))  # what the hell
+t = (10,)
+print(t,type(t))
+
+# Set Data Types
+s1 = {1,2,3}
+s2 = {2,3,1}
+s3 = {3,2,1}
+print(s1==s2==s3)
+print(s1,type(s1))
+s = {10,20,30,10,'shubham',40}
+print(s)    # order duffers everytime I run this line .
+# print(s[0])
+# print(s[1:3])
+s.add(50)
+print(s)
+s.remove(30)
+print(s)
+s = {}    # not an empty set but ____.
+print(s,type(s))
+s = set()   # empty set.
+print(s,type(s))
+
+# Frozen Set
+s = {10,20,30,40}
+fs = frozenset(s)
+print(fs)
+print(type(fs))
+# fs.add(50)
+# fs.remove(20)
+
+# Dict
+d = {100:'shubham',200:'kumar',300:'gupta'}
+print(d)
+print(type(d))
+d = {}
+print(type(d))
+d[400]='skg007'
+print(d)
+d[100]='shubham'
+print((d))
+d[200]='kumar'
+print(d)
+d[300]='gupta'
+print(d)
+# value update in dictionary
+d[100]='abhishek'
+print(d)
